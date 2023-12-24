a16a1bb2c026498894e60a60a0b682d9cc75867b
James Nock <jpnock@gmail.com>
2023-12-24 22:14:54 +0000

    Add handler() header

--- include/cli.h
+++ include/cli.h
@@ -2,8 +2,13 @@
 #define LANGPROC_COMPILER_CLI_H
 
 #include <iostream>
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 
+void handler(int sig);
 int parse_command_line_args(int argc, char **argv, std::string &sourcePath, std::string &outputPath);
 
 #endif
