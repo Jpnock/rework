4741d8ceac9039ae51baeb9f33ead83f5351d941
James Nock <jpnock@gmail.com>
2024-01-25 12:34:27 +0000

    ast: Add a container for a list of expressions

--- include/ast/ast_expression.hpp
+++ include/ast/ast_expression.hpp
@@ -2,10 +2,7 @@
 #define ast_expression_hpp
 
 #include <string>
-#include <iostream>
-#include <map>
-
-#include <memory>
+#include <vector>
 
 class Expression;
 
@@ -28,6 +25,57 @@ public:
     virtual void compile(Context &ctx, std::ostream &dst) const
     {
         throw std::runtime_error("Not implemented.");
+    }
+};
+
+// Represents a list/vector of expressions.
+class ExpressionList
+    : public Expression
+{
+private:
+    std::vector<ExpressionPtr> exprs;
+
+public:
+    ExpressionList(ExpressionPtr firstItem)
+        : exprs({firstItem})
+    {
+    }
+
+    ~ExpressionList()
+    {
+        for (auto expr : exprs)
+        {
+            delete expr;
+        }
+    }
+
+    inline void push_back(ExpressionPtr item)
+    {
+        exprs.push_back(item);
+    }
+
+    virtual void print(std::ostream &dst) const override
+    {
+        for (auto expr : exprs)
+        {
+            if (expr == nullptr)
+            {
+                continue;
+            }
+            expr->print(dst);
+        }
+    }
+
+    virtual void compile(Context &ctx, std::ostream &dst) const override
+    {
+        for (auto expr : exprs)
+        {
+            if (expr == nullptr)
+            {
+                continue;
+            }
+            expr->compile(ctx, dst);
+        }
     }
 };
 
